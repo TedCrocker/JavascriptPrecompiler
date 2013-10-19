9eb39e8b4809e718854cb1b6eb0c58f15b3d5ea8
Ted.Crocker <edward.b.crocker@gmail.com>
2013-10-19 00:16:32 -0400

    Made the dust js compiler more modular.

---
+++ JavascriptPrecompiler/DustJsPrecompiler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Jurassic;
+
+namespace JavascriptPrecompiler
+{
+	public class DustJsPrecompiler
+	{
+		private const string _javascriptLibraryPath = @"Scripts\dust.js";
+		private const string _javascriptCompileFunction = @"dust.compile";
+		private const string _loadTemplateFunction = @"dust.loadSource";
+		private readonly IDictionary<string, string> _filesToLoad = new Dictionary<string, string>();
+		private readonly ScriptEngine _engine;
+
+		public string Output { get; private set; }
+
+		public DustJsPrecompiler()
+		{
+			_engine = new ScriptEngine();
+			_engine.ExecuteFile(_javascriptLibraryPath);
+			_engine.Execute("var precompile = " + _javascriptCompileFunction);
+		}
+
+		public DustJsPrecompiler Add(string templateName, string templateFilePath)
+		{
+			_filesToLoad.Add(templateName, templateFilePath);
+			return this;
+		}
+
+		public DustJsPrecompiler Compile()
+		{
+			var builder = new StringBuilder();
+			builder.AppendLine("(function()\r\n{");
+			foreach (var file in _filesToLoad)
+			{
+				var input = File.ReadAllText(file.Value);
+				var precompileTemplate = _engine.CallGlobalFunction("precompile", input, file.Key);
+				builder.AppendFormat("\t{0}('{1}');\r\n", _loadTemplateFunction, precompileTemplate);
+			}
+			builder.AppendLine("})();");
+
+			Output = builder.ToString();
+			return this;
+		}
+
+	}
+}
--- PrecompilerTests/PrecompilerTests.cs
+++ PrecompilerTests/PrecompilerTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,6 +28,18 @@ namespace PrecompilerTests
 			var precompile = TemplatePrecompiler.PrecompileDustJS("Test", template);
 			Assert.That(precompile, Is.Not.Null);
 			Assert.That(precompile, Is.Not.Empty);
+		}
+
+		[Test]
+		public void DustPrecompilerTest()
+		{
+			var compiler = new DustJsPrecompiler();
+			compiler.Add("test", @"testFiles\\helloWorld.dust");
+			compiler.Compile();
+
+			Assert.That(compiler.Output, Is.Not.Null);
+			Assert.That(compiler.Output, Is.Not.Empty);
+			File.WriteAllText("test.js", compiler.Output);
 		}
 	}
 }
