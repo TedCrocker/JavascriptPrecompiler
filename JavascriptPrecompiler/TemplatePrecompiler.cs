567e8b78d9e9ce3f64da07421ffbb4f0cf58ab8f
Ted.Crocker <edward.b.crocker@gmail.com>
2013-10-18 23:52:19 -0400

    Initial commit

---
+++ JavascriptPrecompiler/TemplatePrecompiler.cs
@@ -0,0 +1,24 @@
+﻿using Jurassic;
+
+
+namespace JavascriptPrecompiler
+{
+	public static class TemplatePrecompiler
+	{
+		public static string PrecompileHandlebars(string name, string template)
+		{
+			var engine = new ScriptEngine();
+			engine.ExecuteFile(@"Scripts\handlebars.js");
+			engine.Execute(@"var precompile = Handlebars.precompile;");
+			return string.Format("var {0} = Handlebars.template({1});", name, engine.CallGlobalFunction("precompile", template).ToString());
+		}
+
+		public static string PrecompileDustJS(string templateName, string template)
+		{
+			var engine = new ScriptEngine();
+			engine.ExecuteFile(@"Scripts\dust.js");
+			engine.Execute(@"var precompile = dust.compile;");
+			return engine.CallGlobalFunction("precompile", template, templateName).ToString();
+		}
+	}
+}
---
+++ PrecompilerTests/PrecompilerTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JavascriptPrecompiler;
+using NUnit.Framework;
+
+namespace PrecompilerTests
+{
+	[TestFixture]
+	public class PrecompilerTests
+	{
+		[Test]
+		public void HandlebarsTest()
+		{
+			var template = "<div>{{hello}} world!</div>";
+			var precompile = TemplatePrecompiler.PrecompileHandlebars("Test", template);
+			Assert.That(precompile, Is.Not.Null);
+			Assert.That(precompile, Is.Not.Empty);
+		}
+
+		[Test]
+		public void DustJSTest()
+		{
+			var template = "<div>{hello} world!</div>";
+			var precompile = TemplatePrecompiler.PrecompileDustJS("Test", template);
+			Assert.That(precompile, Is.Not.Null);
+			Assert.That(precompile, Is.Not.Empty);
+		}
+	}
+}
