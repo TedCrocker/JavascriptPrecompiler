11816cf86c87ad74884d430b4487bb26ddc57dc0
Ted.Crocker <edward.b.crocker@gmail.com>
2013-10-26 15:56:24 -0400

    Added tests for the FileResources class.

---
+++ PrecompilerTests/FileResourcesTests.cs
@@ -0,0 +1,24 @@
+﻿using JavascriptPrecompiler.Utilities;
+using NUnit.Framework;
+
+namespace PrecompilerTests
+{
+	[TestFixture]
+	public class FileResourcesTests
+	{
+		[Test]
+		public void CanGetFileThatExistsOnFileSystem()
+		{
+			var contents = FileResources.GetFileContents("testFiles/helloWorld.dust");
+
+			Assert.That(contents, Is.Not.Empty);
+			Assert.That(contents, Is.StringContaining("Hello {name}!"));
+		}
+
+		[Test]
+		public void CanGetEmbeddedResourceWhenFileDoesNotExist()
+		{
+			var contents = FileResources.GetFileContents("JavascriptPrecompilers/handlebars.js");
+		}
+	}
+}
