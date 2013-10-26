8507ce327338c983590bda916e110419b0a3eefe
Ted.Crocker <edward.b.crocker@gmail.com>
2013-10-26 16:59:39 -0400

    Adding tests for the precompiler.

---
+++ PrecompilerTests/PrecompilerTests.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using JavascriptPrecompiler;
+using JavascriptPrecompiler.Precompilers;
+using NUnit.Framework;
+
+namespace PrecompilerTests
+{
+	[TestFixture]
+	public class PrecompilerTests
+	{
+		private Precompiler _precompiler;
+
+		[SetUp]
+		public void Setup()
+		{
+			_precompiler = new Precompiler(new FakeJSPrecompiler());
+			_precompiler.GetType().GetField("OutputCache", BindingFlags.NonPublic | BindingFlags.Static).SetValue(null, new Dictionary<string, string>());
+		}
+
+		[Test]
+		public void CanAddSingleFile()
+		{
+
+			_precompiler.Add("testTemplate", "testFiles/helloWorld.dust");
+
+			var result = _precompiler.Compile();
+			Assert.That(result, Is.Not.Null);
+			Assert.That(Cache.Count, Is.EqualTo(1));
+			Assert.That(Cache.First().Value, Is.StringContaining("testTemplate"));
+		}
+
+
+		[Test]
+		public void CanAddMultipleFiles()
+		{
+			_precompiler.Add("testFiles/*.dust");
+			var results = _precompiler.Compile();
+
+			Assert.That(results, Is.Not.Null);
+			Assert.That(Cache.Count, Is.EqualTo(1));
+
+			Assert.That(Cache.First().Value, Is.StringContaining("var helloWorld = "));
+			Assert.That(Cache.First().Value, Is.StringContaining("var helloWorld2 = "));
+		}
+
+		private IDictionary<string, string> Cache
+		{
+			get
+			{
+				return _precompiler.GetType().GetField("OutputCache", BindingFlags.NonPublic | BindingFlags.Static).GetValue(_precompiler)
+					as IDictionary<string, string>;
+			}
+		}
+
+
+	}
+
+	public class FakeJSPrecompiler : IPrecompiler
+	{
+		public string GetLibraryRuntimeFileContents()
+		{
+			return "testString";
+		}
+
+		public string GetJavascript(string templateName, string template)
+		{
+			return "var " + templateName + " = '" + template + "';";
+		}
+	}
+}
